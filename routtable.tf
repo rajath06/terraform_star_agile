37a767c89210fcd8b62f80d977bb613cd14ea652
rajath06 <83548992+rajath06@users.noreply.github.com>
2025-03-08 16:49:57 +0530

    Create routtable.tf

---
+++ routtable.tf
@@ -0,0 +1,17 @@
+resource "aws_route_table" "main_route_table" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main_igw.id
+  }
+
+  tags = {
+    Name = "MainRouteTable"
+  }
+}
+
+resource "aws_route_table_association" "main_rta" {
+  subnet_id      = aws_subnet.main_subnet.id
+  route_table_id = aws_route_table.main_route_table.id
+}
