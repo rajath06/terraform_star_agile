50d3cb4cd81e5e1274a41b00de7e5b9a13ece68c
rajath06 <83548992+rajath06@users.noreply.github.com>
2025-03-08 16:48:43 +0530

    Create vpc.tf

---
+++ vpc.tf
@@ -0,0 +1,17 @@
+resource "aws_vpc" "main_vpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "MainVPC"
+  }
+}
+
+resource "aws_subnet" "main_subnet" {
+  vpc_id                  = aws_vpc.main_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-west-1a" 
+  tags = {
+    Name = "MainSubnet"
+  }
+}
