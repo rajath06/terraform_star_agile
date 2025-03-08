915d03b983632b3c1671bbb6d2dd3aaeb9fac0dd
rajath06 <83548992+rajath06@users.noreply.github.com>
2025-03-08 16:51:28 +0530

    Create security.tf

---
+++ security.tf
@@ -0,0 +1,29 @@
+resource "aws_security_group" "main_sg" {
+  vpc_id = aws_vpc.main_vpc.id
+  name   = "allow_ssh_http"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allow SSH from anywhere (Modify for security)
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allow HTTP
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"] # Allow all outbound traffic
+  }
+
+  tags = {
+    Name = "MainSG"
+  }
+}
